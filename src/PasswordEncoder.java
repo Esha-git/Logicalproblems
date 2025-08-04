b5c3a99f280b13040dc4d9e65b4180b9e88eb52f
esha <peshajain14@gmail.com>
2025-08-04 14:33:47 +0530

    8 programs changed

---
+++ src/PasswordEncoder.java
@@ -0,0 +1,28 @@
+public class PasswordEncoder {
+
+        public static String encode(String password) {
+            StringBuilder encoded = new StringBuilder();
+
+            for (char ch : password.toCharArray()) {
+                char upper = Character.toUpperCase(ch);
+
+                if ("AEIOU".indexOf(upper) != -1) {
+                    encoded.append('@');
+                } else if (Character.isDigit(upper) && (upper - '0') % 2 == 0) {
+                    encoded.append('*');
+                } else {
+                    encoded.append(upper);
+                }
+            }
+
+            return encoded.toString();
+        }
+
+        public static void main(String[] args) {
+            String input = "Java1234";
+            String output = encode(input);
+            System.out.println(output);  // Output: J@V@1*3*
+        }
+    }
+
+
